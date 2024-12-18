103017735e85919760c15618d61ca88e9e52aefb
busenurdmb <bus.dmb@gmail.com>
2024-12-18 21:02:23 +0300

    adım 2 -->18.12.2024

---
+++ AcunMedyaAkademiPortfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+       DbPortfolioEntities3 db=new DbPortfolioEntities3();
+        public ActionResult Index()
+        {
+            var values = db.TblTestimonial.ToList();
+            return View(values);
+        }
+    }
+}
+//tolist--> select * from tablename
