8c69b63e04b14db05262954239b405a1ed62a7f7
busenurdmb <bus.dmb@gmail.com>
2024-12-18 21:13:52 +0300

    adım 3-->18.12.2024

---
+++ AcunMedyaAkademiPortfolio/Controllers/SkillController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbPortfolioEntities3 db=new DbPortfolioEntities3();
+        public ActionResult SkillList()
+        {
+            var values = db.TblSkill.ToList();
+            return View(values);
+        }
+    }
+}
