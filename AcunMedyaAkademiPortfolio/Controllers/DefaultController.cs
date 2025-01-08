ef7c6ca263ac0053c39add6785d2a4378fe2899b
busenurdmb <bus.dmb@gmail.com>
2025-01-08 20:48:36 +0300

    adım 3  > 08.01.2025

---
+++ AcunMedyaAkademiPortfolio/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolio.Models;
+namespace AcunMedyaAkademiPortfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbPortfolioEntities3 db = new DbPortfolioEntities3();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialFeature()
+        {
+            var values = db.TblFeature.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblAbout.ToList();
+            return PartialView(values);
+        }
+    }
+}
